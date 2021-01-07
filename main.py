0c4f623c714f81ff1af3c5684e2ce240f8d79731
SANOGO <SOULEYSANOGO@gs2e.ci>
2021-01-07 14:29:13 +0000

    Update

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from typing import Optional
+from typing import List, Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+    tags: List[str] = []
+
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item):
+    return item
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
